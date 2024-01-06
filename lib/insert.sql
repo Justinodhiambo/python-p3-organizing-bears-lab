980599b9280194847f102196dc82591087e64c22
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2024-01-06 05:53:59 -0500

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 7, 'M', 'Black', 'Aggressive', 0),
+('Tabitha', 3, 'F', 'Blonde', 'Playful', 1),
+('Sergeant Brown', 10, 'M', 'Brown', 'Stern', 1),
+('Melissa', 4, 'F', 'Black', 'Shy', 1),
+('Grinch', 6, 'M', 'Green', 'Grumpy', 0),
+('Wendy', 2, 'F', 'White', 'Curious', 1),
+(NULL, NULL, 'F', NULL, NULL, NULL); -- unnamed bear
